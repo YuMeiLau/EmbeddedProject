ffb0fd15978ff121b5765aa69d0bf8ce89e7abc5
Elliot M Fish <efish@mario.ece.utexas.edu>
2017-11-22 13:39:00 -0600

    added metrics.sh

---
+++ metrics.sh
@@ -0,0 +1,11 @@
+#ifndef _METRICS_SH_
+#define _METRICS_SH_
+
+/* Counters/Metrics used to evaluate algorithm */
+
+unsigned long _COLLISIONS
+unsigned long _PACKETS_SENT
+unsigned long _PACKETS_DROPPED
+
+
+#endif
