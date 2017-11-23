10eb9af99a9255cefd1214180065d5eb5c92040f
Elliot M Fish <efish@mario.ece.utexas.edu>
2017-11-22 18:07:44 -0600

    added nic.sc and metrics.c

---
+++ metrics.c
@@ -0,0 +1,8 @@
+#include "metrics.sh"
+/* Counters/Metrics used to evaluate algorithm */
+
+unsigned long _COLLISIONS = 0;
+unsigned long _PACKETS_SENT = 0;
+unsigned long _PACKETS_DROPPED = 0;
+unsigned long _WB_OUTGOING_DATA_DROPPED = 0;
+
